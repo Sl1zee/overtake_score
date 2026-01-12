5ae4899dfa0ea888a32ffc8dcd56d04b04c32f83
Sl1zee <dtoropccin@gmail.com>
2026-01-12 21:07:16 +0300

    Update overtake_score.lua

--- overtake_score.lua
+++ overtake_score.lua
@@ -241,7 +241,7 @@ local speedWarning = 0
            end
        end
 
-        ui.beginTransparentWindow("overtakeScore", vec2(100, 100), vec2(400, 400))
+        ui.beginTransparentWindow("overtakeScore", vec2(100, 100), vec2(600, 450))
         ui.beginOutline()
 
         ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
@@ -294,6 +294,7 @@ local speedWarning = 0
         ui.endTransparentWindow()
 
     end
+
 
 
 
